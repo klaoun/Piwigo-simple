f8f48b9e4f040deb53dae700de151696b0f762e8
plegall <plg@piwigo.org>
2012-03-14 13:50:34 +0000

    import theme Simple version 2.5
    * use media queries to adapt layout for small screens
    * remove arbitrary limit on width: the picture will simply be partially hidden if too wide
    * update password.tpl
    * add MENUBAR on all pages
    * Spelling correction in README
    * update url
    * bugfix for the feature 'open external links in new window' for menubar links 
    
    
    git-svn-id: http://piwigo.org/svn/extensions/simple_themes/simple@13555 68402e56-0260-453c-a942-63ccdbb3a9ee

--- themeconf.inc.php
+++ themeconf.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Theme Name: Simple Grey
-Version: 2.4.3
+Version: 2.5
 Description: Simple Grey
 Theme URI: http://piwigo.org/ext/extension_view.php?eid=308
 Author: Saïmon
@@ -18,11 +18,12 @@ $themeconf = array(
   'admin_icon_dir' => 'themes/default/icon/admin',
   'mime_icon_dir' => 'themes/default/icon/mimetypes/',
   'local_head' => 'local_head.tpl',
-  'url' => 'http://saimon.org/blog/pages/Theme-Simple-Grey-Piwigo'
+  'url' => 'http://saimon.org/log/theme-simple-grey-pour-piwigo.html'
 );
 
-global $user;
-$user['maxwidth'] = 720;
+// set this if you want to restrict the width of pictures to a specifc width
+// global $user;
+// $user['maxwidth'] = 800;
 
 load_language('theme.lang', PHPWG_THEMES_PATH.'simple/');
 

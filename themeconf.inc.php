7cb00ec35f8164c3b81557b6000d01e26fdcb15f
plegall <plg@piwigo.org>
2012-03-14 13:28:10 +0000

    import theme Simple version 2.1
    - bugfix for user's profile - use default `profile_content.tpl`
    - child themes do not need to have `content.css`
    - use `local_head.tpl`
    - correct order of thumbnails & show copyright on picture page
    - use default as parent theme & change theme URI
    - add quicksearch and style input, textarea, ...
    - add login link in the footer
    
    
    git-svn-id: http://piwigo.org/svn/extensions/simple_themes/simple@13547 68402e56-0260-453c-a942-63ccdbb3a9ee

--- themeconf.inc.php
+++ themeconf.inc.php
@@ -3,18 +3,21 @@
 Theme Name: Simple Grey
 Version: 2.0
 Description: Simple Grey theme
-Theme URI: http://saimon.org/blog/pages/Theme-Simple-Grey-Piwigo
+Theme URI: http://piwigo.org/ext/extension_view.php?eid=308
 Author: Saïmon
 Author URI: http://saimon.org/
 */
 
 $themeconf = array(
-  /* 'parent' => 'default', */
+  'parent' => 'default',
+  'load_parent_css' => false,
+  'load_parent_local_head' => false,
   'name' => 'simple-grey',
   'theme_dir' => 'simple',
   'icon_dir' => 'themes/simple/icon',
   'admin_icon_dir' => 'themes/default/icon/admin',
   'mime_icon_dir' => 'themes/default/icon/mimetypes/',
+  'local_head' => 'local_head.tpl',
   'url' => 'http://saimon.org/blog/pages/Theme-Simple-Grey-Piwigo'
 );
 

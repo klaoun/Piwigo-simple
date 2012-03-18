11af64f04952c819ee89911dbd86afb395ec1dc7
ddtddt <ddtddt@piwigo.org>
2012-03-18 18:32:37 +0000

    [extensions] - simple_themes - add es_ES - Español – Spanish - Thanks to jpr928
    git-svn-id: http://piwigo.org/svn/extensions/simple_themes/simple@13625 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/es_ES/theme.lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['Show/hide menu'] = 'Mostrar / ocultar el menú';
+$lang['Album title and description'] = 'Título y descripción del álbum';
+$lang['under the album thumbnail'] = 'bajo la miniatura representante';
+$lang['next to the album thumbnail'] = 'junto a la miniatura representante';
+
+?>

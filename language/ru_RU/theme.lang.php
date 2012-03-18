c97b18aa5469e7955b19e6b74ecb414d6afaa3b3
ddtddt <ddtddt@piwigo.org>
2012-03-18 19:54:57 +0000

    [extensions] - simple_themes - add ru_RU - Русский - Russian - Thanks to ie
    git-svn-id: http://piwigo.org/svn/extensions/simple_themes/simple@13629 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/theme.lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['Show/hide menu'] = 'Показать/скрыть меню';
+$lang['Album title and description'] = 'Название и описание альбома';
+$lang['under the album thumbnail'] = 'под уменьшенное изображение альбома';
+$lang['next to the album thumbnail'] = 'рядом с уменьшенным изображением альбома';
+
+?>

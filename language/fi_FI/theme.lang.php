0703f670c7e01391f4c46d91efdbf7c78c82ea9d
ddtddt <ddtddt@piwigo.org>
2012-03-25 14:40:36 +0000

    [extensions] - simple - add fi_FI  – Finnish - Thanks to Jussi Sunnari
    git-svn-id: http://piwigo.org/svn/extensions/simple_themes/simple@13717 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fi_FI/theme.lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['Show/hide menu'] = 'Näytä/Piilota valikko';
+$lang['Album title and description'] = 'Almumin otsikko ja kuvaus';
+$lang['under the album thumbnail'] = 'albumin avaus kuvake';
+$lang['next to the album thumbnail'] = 'seuraavaan albumiin kuvake';
+
+?>

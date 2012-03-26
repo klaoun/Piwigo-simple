96ad509ccc8f3ab2a77f93ee6081bafcb14a571a
ddtddt <ddtddt@piwigo.org>
2012-03-26 07:21:21 +0000

    [extensions] - simple_themes - add pt_PT - Português - Portuguese - Thanks to alahel76
    git-svn-id: http://piwigo.org/svn/extensions/simple_themes/simple@13739 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_PT/theme.lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['Show/hide menu'] = 'Afixar/esconder o menu';
+$lang['Album title and description'] = 'Titulo e descrição do álbum';
+$lang['under the album thumbnail'] = 'Sobe a miniatura representante';
+$lang['next to the album thumbnail'] = 'Ao lado da miniatura representante';
+
+?>

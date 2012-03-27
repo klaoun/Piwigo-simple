826d7cca354a70d60eb121ec7d4dc3cff4924278
ddtddt <ddtddt@piwigo.org>
2012-03-27 18:44:56 +0000

    [extensions] - simple_themes - add sk_SK – Slovensky – Slovak - Thanks to dodo
    git-svn-id: http://piwigo.org/svn/extensions/simple_themes/simple@13768 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/theme.lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['Show/hide menu'] = 'Ukázať/schovať menu';
+$lang['Album title and description'] = 'Názov albumu a popis';
+$lang['under the album thumbnail'] = 'pod náhľadom albumu';
+$lang['next to the album thumbnail'] = 'vedľa náhľadu albumu';
+
+?>

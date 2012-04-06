f7bf4b647846d747a392f180cf8d259ed0892a20
ddtddt <ddtddt@piwigo.org>
2012-04-06 17:46:44 +0000

    [extensions] - simple - add lv_LV - Latviešu - Latvian - Thanks to Aivars Baldone
    git-svn-id: http://piwigo.org/svn/extensions/simple_themes/simple@13938 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/theme.lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['Show/hide menu'] = 'Rādīt/paslēpt izvēlni';
+$lang['Album title and description'] = 'Albūma nosaukums un apraksts';
+$lang['under the album thumbnail'] = 'zem albūma sīkbildes';
+$lang['next to the album thumbnail'] = 'blakus albūma sīkbildei';
+
+?>

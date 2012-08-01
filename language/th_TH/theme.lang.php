91d91fd9161f333faa995fb98c3375e86779380d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-01 05:57:15 +0000

    [simple] Update th_TH, thanks to :
    git-svn-id: http://piwigo.org/svn/extensions/simple_themes/simple@17252 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/th_TH/theme.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Album title and description'] = 'ชื่ออัลบั้มและคำอธิบาย';
+$lang['Show/hide menu'] = 'แสดง / ซ่อนเมนู';
+$lang['next to the album thumbnail'] = 'รูปขนาดย่อของอัลบั้ม ถัดไป';
+$lang['under the album thumbnail'] = 'ภายใต้รูปขนาดย่อของอัลบั้ม';
+?>

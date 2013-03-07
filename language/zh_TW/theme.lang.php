191ae7d44adb2fafd1f5b6e18cd3bc125c824784
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-07 15:03:31 +0000

    [simple] Add zh_TW, thanks to : tedx
    git-svn-id: http://piwigo.org/svn/extensions/simple_themes/simple@21315 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/theme.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Album title and description'] = '專輯的標題和說明';
+$lang['Show/hide menu'] = '顯示/隱藏選單';
+$lang['next to the album thumbnail'] = '相簿縮圖的旁邊';
+$lang['under the album thumbnail'] = '相冊縮圖之下';
+?>

f65ce56e78d600211235f04c0026f576681d8977
ddtddt <ddtddt@piwigo.org>
2013-08-06 06:29:39 +0000

    [extensions] - simple -Add vi_VN - Tiếng Việt - Vietnamese - Thanks to Em Ku
    git-svn-id: http://piwigo.org/svn/extensions/simple_themes/simple@24111 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/vi_VN/theme.lang.php
@@ -0,0 +1,6 @@
+<?php
+$lang['Show/hide menu'] = 'Hiện/ẩn trình đơn';
+$lang['Album title and description'] = 'Tiêu đề album và mô tả';
+$lang['under the album thumbnail'] = 'dưới ảnh thu nhỏ của album';
+$lang['next to the album thumbnail'] = 'kế tiếp ảnh thu nhỏ của album';
+?>

b9ac0afac9549ee7d68beb6458a17cb293d46644
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-07-18 07:54:00 +0200

    Update sk_SK, thanks to jose1711

--- language/sk_SK/theme.lang.php
+++ language/sk_SK/theme.lang.php
@@ -1,8 +1,6 @@
 <?php
 
-$lang['Show/hide menu'] = 'Ukázať/schovať menu';
+$lang['Show/hide menu'] = 'Ukázať/skryť menu';
 $lang['Album title and description'] = 'Názov albumu a popis';
 $lang['under the album thumbnail'] = 'pod náhľadom albumu';
 $lang['next to the album thumbnail'] = 'vedľa náhľadu albumu';
-
-?>

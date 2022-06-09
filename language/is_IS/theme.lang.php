9c2b85d88cb4c9e08f16b7c67b9cc56d51eb408c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-06-09 06:31:51 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/theme.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Album title and description'] = 'Heiti albúms og lýsing';
+$lang['Show/hide menu'] = 'Sýna/fela valmynd';
+$lang['next to the album thumbnail'] = 'við hliðina á smámynd albúmsins';
+$lang['under the album thumbnail'] = 'undir smámynd albúmsins';
